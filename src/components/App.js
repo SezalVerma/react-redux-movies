7bc5ca15766b1fdc379cb2a8bc56f5da47e1d9d3
sezalverma <sezal.verma@nagarro.com>
2021-11-15 11:49:47 +0530

    Added Data.js , css , Navbar & MovieCard components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
+import date, { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 function App() {
-  return <div className="App">Movies</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div class="tabs">
+          <div class="tab">Movies</div>
+          <div class="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
