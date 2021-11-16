649cd50ee1267951685a79ba317cda5f58d85816
sezalverma <sezal.verma@nagarro.com>
2021-11-16 13:06:37 +0530

    Middleware added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// curried function for - logger(obj,next,action)
+// redux would pass obj {dispatch,getState} to logger
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log("ACTION_TYPE = ", action.type);
+      // pass the flow of code to - action
+      next(action);
+    };
+  };
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
